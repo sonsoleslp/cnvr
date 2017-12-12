5c79179d79c7dfa4483b73b406d17a1e447ccb6b
pablo17993 <pablo17993@gmail.com>
2017-12-12 18:30:52 +0100

    /home

--- scripts/lab/zk-setup.sh
+++ scripts/lab/zk-setup.sh
@@ -13,8 +13,8 @@ cd ~/cnvr/zookeeper-3.4.10
 
 export CLASSPATH=$CLASSPATH:~/cnvr/zookeeper-3.4.10/zookeeper-3.4.10.jar
 export CLASSPATH=$CLASSPATH:~/cnvr/zookeeper-3.4.10/lib/*
-export PATH=$PATH:/cnvr/zookeeper-3.4.10/bin
-./bin/zkServer.sh start ~/cnvr/scripts/zoo1.cfg
-./bin/zkServer.sh start ~/cnvr/scripts/zoo2.cfg
-./bin/zkServer.sh start ~/cnvr/scripts/zoo3.cfg
+export PATH=$PATH:~/cnvr/zookeeper-3.4.10/bin
+./bin/zkServer.sh start ~/cnvr/zk/zoo1.cfg
+./bin/zkServer.sh start ~/cnvr/zk/zoo2.cfg
+./bin/zkServer.sh start ~/cnvr/zk/zoo3.cfg
 sleep 20
