2b4bc356d850a971b3bdc6a33147d1a8eaa7b270
pablo17993 <pablo17993@gmail.com>
2017-12-13 18:49:16 +0100

    ZK

--- scripts/lab/distributed-zk-setup.sh
+++ scripts/lab/distributed-zk-setup.sh
@@ -4,21 +4,14 @@ tar --extract --file zookeeper-3.4.10.tar.gz
 cd ~/cnvr/zookeeper-3.4.10
 
 mkdir /tmp/1
-mkdir /tmp/2
-mkdir /tmp/3
 echo 1 > /tmp/1/myid
-echo 2 > /tmp/2/myid
-echo 3 > /tmp/3/myid
 
 cd ~/cnvr/zookeeper-3.4.10
-cp ~/cnvr/zk/zoo1.cfg conf/zoo1.cfg
-cp ~/cnvr/zk/zoo2.cfg conf/zoo2.cfg
-cp ~/cnvr/zk/zoo3.cfg conf/zoo3.cfg
+cp ~/cnvr/zk/zoo_sample.cfg conf/zoo_sample.cfg
+
 
 export CLASSPATH=$CLASSPATH:~/cnvr/zookeeper-3.4.10/zookeeper-3.4.10.jar
 export CLASSPATH=$CLASSPATH:~/cnvr/zookeeper-3.4.10/lib/*
 export PATH=$PATH:~/cnvr/zookeeper-3.4.10/bin
-./bin/zkServer.sh start conf/zoo1.cfg
-./bin/zkServer.sh start conf/zoo2.cfg
-./bin/zkServer.sh start conf/zoo3.cfg
- 
+./bin/zkServer.sh start conf/zoo_sample.cfg
+
